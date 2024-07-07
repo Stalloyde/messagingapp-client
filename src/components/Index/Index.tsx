8c06c8d1ecfa524194328dbc5156112c08216acd
Stalloyde <stalloyde93@gmail.com>
2024-07-07 21:56:49 +0800

    Change text on main screen

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,7 +1,7 @@
 import styles from './Index.module.css';
 
 function Index() {
-  return <div className={styles.welcome}>Loading The Messaging App!</div>;
+  return <div className={styles.welcome}>Welcome to the Messaging App!</div>;
 }
 
 export default Index;
